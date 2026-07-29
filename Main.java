7797981d033995ed957fc45bbcd202ac969140fc
杜文斌 <857617704@qq.com>
2026-07-29 17:18:11 +0800

    feat: 新增异常

---
+++ Main.java
@@ -0,0 +1,22 @@
+// exception
+public class Main {
+    public static void main(String[] args) {
+        try {
+            process1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void process1() {
+        try {
+            process2();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static void process2() {
+        throw new NullPointerException();
+    }
+}
