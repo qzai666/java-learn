1d8596282c70f2a4a3e9b20505c270e475ddcc3a
杜文斌 <857617704@qq.com>
2026-09-09 16:23:03 +0800

    feat: 修复时间显示

--- springboot-web/src/main/java/com/heima/pojo/User.java
+++ springboot-web/src/main/java/com/heima/pojo/User.java
@@ -2,6 +2,8 @@ package com.heima.pojo;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,7 @@ public class User {
     private String password;
     private String name;
     private Integer age;
+    @JsonFormat(pattern = "MM-dd HH:mm:ss")
     private LocalDateTime upDateTime;
 
 }
