b4f22bd35f174cc09ec34e1c62bbcdd2387b5043
杜文斌 <857617704@qq.com>
2026-09-10 16:08:40 +0800

    feat: 数据库

---
+++ springboot-mybatis-test/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,23 @@
+package com.heima.pojo;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+    private Integer age;
+    @JsonFormat(pattern = "MM-dd HH:mm:ss")
+    private LocalDateTime upDateTime;
+
+}
